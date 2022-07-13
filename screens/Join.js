c663c529588122fae64cba544b1bfbf59758468d
ahnniething <ahnniething@gmail.com>
2022-07-13 16:30:37 +0900

    #3 feat: configure navigation and link screens depending on the authentication

---
+++ screens/Join.js
@@ -0,0 +1,19 @@
+import React, { useRef, useState } from "react";
+import styled from "styled-components/native";
+import { BLACK_COLOR } from "../colors";
+
+const Container = styled.View`
+  background-color: ${BLACK_COLOR};
+  flex: 1;
+  align-items: center;
+  color: white;
+  padding: 60px 20px;
+`;
+
+const Join = () => {
+  return (
+    <Container>
+    </Container>
+  );
+};
+export default Join;

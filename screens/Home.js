930cde78996b49ef16f3811e64a82a5340fb4c5c
ahnniething <ahnniething@gmail.com>
2022-07-14 14:21:33 +0900

    #5 feat: display coin list calling the coinpaprika API

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList } from "react-native";
+import { useQuery } from "react-query";
 import styled from "styled-components/native";
+import { coins } from "../api";
+import { BLACK_COLOR } from "../colors";
 
-const Container = styled.View``;
+const Container = styled.View`
+  background-color: ${BLACK_COLOR};
+  flex: 1;
+`;
+
 const Text = styled.Text``;
-const Home = () => (
-  <Container>
-    <Text>Home</Text>
-  </Container>
-);
+const Loader = styled.View`
+  flex: 1;
+  background-color: ${BLACK_COLOR};
+  justify-content: center;
+  align-items: center;
+`;
+
+const Coin = styled.View`
+  align-items: center;
+`;
+const CoinName = styled.Text`
+  color: white;
+`;
+const CoinSymbol = styled.Text`
+  color: white;
+`;
+const Home = () => {
+  const { isLoading, data } = useQuery("coins", coins);
+  const [cleanData, setCleanData] = useState([]);
+  useEffect(() => {
+    setCleanData(data.filter(coin => coin.rank != 0 && coin.is_active && !coin.is_new));
+    console.log(data.length, cleanData.length);
+  }, [data]);
+  if (isLoading) {
+    return (
+      <Loader>
+        <ActivityIndicator color="white" />
+      </Loader>
+    );
+  }
+  return (
+    <Container>
+      <FlatList
+        data={cleanData}
+        numColumns={5}
+        keyExtractor={(item) => item.id+""}
+        renderItem={({ item }) => (
+          <Coin>
+            <CoinName>{item.name}</CoinName>
+            <CoinSymbol>{item.symbol}</CoinSymbol>
+          </Coin>
+        )}
+      />
+    </Container>
+  );
+};
+
 export default Home;

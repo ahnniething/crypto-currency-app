b5e7debea85138514c3c1149bf3965af5984843a
ahnniething <ahnniething@gmail.com>
2022-07-14 11:27:19 +0900

    #3 feat: add error handling  when signing up through firebase authentication

--- screens/Join.js
+++ screens/Join.js
@@ -1,7 +1,9 @@
 import React, { useRef, useState } from "react";
 import styled from "styled-components/native";
 import { BLACK_COLOR } from "../colors";
-import {TextInput, Btn, BtnText} from '../components/common'
+import { TextInput, Btn, BtnText } from "../components/common";
+import auth from "@react-native-firebase/auth";
+import { ActivityIndicator, Alert } from "react-native";
 
 const Container = styled.View`
   background-color: ${BLACK_COLOR};
@@ -15,13 +17,65 @@ const Join = () => {
   const passwordInput = useRef();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const onSubmitEditing = () => {
-   passwordInput.current.focus();
+  const [loading, setLoading] = useState(false);
+  const onSubmitEmailEditing = () => {
+    passwordInput.current.focus();
+  };
+  const onSubmitPasswordEditing = async () => {
+    if (email === "" || password === "") {
+      return Alert.alert("Fill in the form please");
+    }
+    //in case the user clicks more than one time, prevent executing the below code
+    if (loading) {
+      return;
+    }
+    try {
+      const userCredential = await auth().createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(userCredential);
+      /** userCredential
+      {
+        "additionalUserInfo": {
+            "isNewUser": true,
+            "profile": null,
+            "providerId": "password",
+            "username": null
+        },
+        "user": {
+            "displayName": null,
+            "email": "ahnniething@crypto.com",
+            "emailVerified": false,
+            "isAnonymous": false,
+            "metadata": [Object
+            ],
+            "phoneNumber": null,
+            "photoURL": null,
+            "providerData": [Array
+            ],
+            "providerId": "firebase",
+            "refreshToken": "AOEOulaQ5J7_yGfmfqch5I00wPQG4_0jRSxuzc7KWZ7QrBGyMrucTkIeIDUnjNCgSbY1rB2x1bOjydljSP19BH44ltDnK4SW8uFukWiMXyW4zNaHukoiqGaooUTRKNiwaPO5_utOvJ2FzqTsOvcAUXHrwZQeVXVbTcehZ9PzOhfA5bPe-pZfts5Ge6uXS2mxZgaStg5J6PcEel2_0MOL2ToZsk028FyQ2Q",
+            "tenantId": null,
+            "uid": "AkW7mkfOA5W1nwo2Udf83742HJj1"
+                }
+            }
+        */
+    } catch (e) {
+      switch (e.code) {
+        case "auth/weak-password": {
+          Alert.alert("Write a stronger password!");
+        }
+        case "auth/invalid-email": {
+          Alert.alert("The email address is badly formatted");
+        }
+      }
+    }
   };
 
   return (
     <Container>
-        <TextInput
+      <TextInput
         placeholder="Email"
         autoCapitalize="none"
         autoCorrect={false}
@@ -29,7 +83,7 @@ const Join = () => {
         value={email}
         returnKeyType="next"
         onChangeText={(text) => setEmail(text)}
-        onSubmitEditing={onSubmitEditing}
+        onSubmitEditing={onSubmitEmailEditing}
         placeholderTextColor={"rgba(255, 255, 255, 0.7)"}
       />
       <TextInput
@@ -39,10 +93,15 @@ const Join = () => {
         value={password}
         returnKeyType="done"
         onChangeText={(text) => setPassword(text)}
+        onSubmitEditing={onSubmitPasswordEditing}
         placeholderTextColor={"rgba(255, 255, 255, 0.7)"}
       />
-      <Btn>
-        <BtnText>Create Account</BtnText>
+      <Btn onPress={onSubmitPasswordEditing}>
+        {loading ? (
+          <ActivityIndicator color={"white"} />
+        ) : (
+          <BtnText>Create Account</BtnText>
+        )}
       </Btn>
     </Container>
   );

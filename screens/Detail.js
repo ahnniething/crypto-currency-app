2af77d48a3ab19d8f9e81470c5d7e7d7c95cceeb
ahnniething <ahnniething@gmail.com>
2022-07-16 01:35:26 +0900

    #7 feat: change data for chart on the detail screen

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,26 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components/native";
 import { Icon } from "../components/common";
 import { useQuery } from "react-query";
 import { history, info } from "../api";
+import { BLACK_COLOR } from "../colors";
 
-const Container = styled.ScrollView``;
+const Container = styled.ScrollView`
+  background-color: ${BLACK_COLOR};
+`;
 
 const Detail = ({
   navigation,
   route: {
-    params: { symbol, id }
-  }
+    params: { symbol, id },
+  },
 }) => {
   useEffect(() => {
     navigation.setOptions({
       headerTitle: () => (
         <Icon
           source={{
-            uri: `https://coinicons-api.vercel.app/api/icon/${symbol.toLowerCase()}`
+            uri: `https://coinicons-api.vercel.app/api/icon/${symbol.toLowerCase()}`,
           }}
         />
-      )
+      ),
     });
   }, []);
 
@@ -32,8 +35,17 @@ const Detail = ({
     ["coinHistory", id],
     history
   );
-
-  console.log(infoData);
+  const [victoryData, setVictoryData] = useState([]);
+  useEffect(() => {
+    if (historyData) {
+      setVictoryData(
+        historyData.map((price) => ({
+          x: new Date(price.timestamp).getTime(),
+          y: price.price,
+        }))
+      );
+    }
+  });
   return <Container />;
 };
 export default Detail;
